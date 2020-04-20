42e800ea548f257fa77daf5b1da332cd204398d6
Vitória <vicknogueira199@gmail.com>
2020-04-20 13:15:27 -0300

    Criação dos componentes navbar, footer, home e contato

--- layout/src/app/app.module.ts
+++ layout/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
